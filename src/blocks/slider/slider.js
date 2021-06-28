abe7ac40d1bc40a4ea884dd51a3dfa071a4b4c33
SoslanZS <79465431+SoslanZS@users.noreply.github.com>
2021-06-28 15:38:57 +0300

    fixed bug

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,23 +1,36 @@
-o2.slider =
+let sliderUl = $('._slider-list'),
+	sliderImg = sliderUl.find('img'),
+	imgWidth = sliderImg[0].width,
+	imgLenght = sliderImg.length , //6
+	index = 0,
+	loc = 0;
+	locL = 0;
+
+function sliderRight(value)
 {
-	slideTo($this)
+	index++;
+	loc += imgWidth ;
+	if(index === imgLenght)
 	{
-		let sliderUl = $('._slider-list');
-		let sliderLi = sliderUl.find('li');
-		let liWidth = sliderLi[0].width; //750px
-		let liLenght = sliderLi.length - 1; //6-1=5
-		let current = 1;
-		let sumWidthLi = liWidth * liLenght; //4500px
-		let sliderBtn = $($this).data('dir');
-
-		if(sliderBtn =='next')
-		{
-			current+=5;
-			console.log(current)
-		}else
-		{
-			current--;
-		}
-
+		index = 0;
+		loc = 0;
 	}
+	sliderUl.animate({
+		'margin-left':`-${loc}px`
+	}, 500)
+}
+
+function sliderLeft(value)
+{
+	index--;
+	loc -= imgWidth;
+	if(index < 0)
+	{
+		index = imgLenght - 1;
+		loc = imgWidth * (imgLenght - 1);
+	}
+
+	sliderUl.animate({
+		'margin-left':'-'+loc+'px'
+	},500)
 }
