07f59dda71152fea7e5688383f8edf35018aee03
SoslanZS <79465431+SoslanZS@users.noreply.github.com>
2021-07-01 11:37:01 +0300

    fixed bug

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -2,13 +2,14 @@ let sliderUl = $('._slider-list'),
 	sliderImg = sliderUl.find('img'),
 	imgWidth = sliderImg[0].width,
 	imgLenght = sliderImg.length,
-	sliderDots = addElement('._slider__dots', "<div class='slider__dots-line'>", imgLenght),
-	dots = $(checkChildren(sliderDots,'div')[0]).append("<div class='slider__dots-line slider__dots-active _slider__dots-active'>"),
+	sliderDots = addElement('._slider__dots', '<div class="slider__dots-line _slider__dots-line" onclick="dotsClick(this)">', imgLenght),
+	dots = $(checkChildren(sliderDots,'div')[0]).append('<div class="slider__dots-active _slider__dots-active">'),
 	dotsActive = $('._slider__dots-active'),
 	dotsWidth = dotsActive.width() + 5,
 	index = 0,
 	locUl = 0,
 	locDots = 0;
+
 
 function sliderRight(value)
 {
@@ -21,12 +22,8 @@ function sliderRight(value)
 		locUl = 0;
 		locDots = 0;
 	}
-	sliderUl.animate({
-		'margin-left':`-${locUl}px`
-	}, 500)
-	dotsActive.animate({
-		'margin-left':`${locDots}px`
-	}, 500)
+
+	sliderMovement()
 }
 
 function sliderLeft(value)
@@ -41,23 +38,45 @@ function sliderLeft(value)
 		locDots = dotsWidth * (imgLenght - 1);
 	}
 
+	sliderMovement()
+}
+
+function dotsClick(value)
+{
+	let imgWidthDots = sliderImg[0].width,
+	dotsWidthDots = dotsActive.width() + 5,
+	dots = $(value).data('num');
+	index = dots;
+	console.log(dots);
+	locUl = imgWidthDots * dots;
+	locDots = dotsWidth * dots;
+
+	sliderMovement()
+}
+
+function sliderMovement()
+{
 	sliderUl.animate({
 		'margin-left':`-${locUl}px`
-	},500)
+	}, 500)
 	dotsActive.animate({
 		'margin-left':`${locDots}px`
-	},500)
+	}, 500)
 }
 
 function addElement(parent,clas,length)
 {
 	let element = $(parent);
-
 	for(let i = 0; i < length; i++)
 	{
 		element.append(clas);
 	}
 
+	let data = element.children();
+	for(let i = 0; i < length; i++)
+	{
+		$(data[i]).attr('data-num', `${i}`);
+	}
 	return element;
 }
 
