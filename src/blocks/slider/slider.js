127054bb448fea9b4937b0bc74ae1de637e614bb
SoslanZS <79465431+SoslanZS@users.noreply.github.com>
2021-07-06 11:09:46 +0300

    fixed bug

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -2,10 +2,11 @@ o2.slider =
 {
 	sliderUl : $('._slider-list'),
 	sliderImg : [],
-	imgWidth : 0,
 	imgLenght : 0,
 	sliderDots : [],
+	sliderDotsLine : [],
 	dots : [],
+	dotsBefore : [],
 	dotsActive : '',
 	dotsWidth: 0,
 	index : 0,
@@ -14,17 +15,22 @@ o2.slider =
 	init()
 	{
 		this.sliderImg = this.sliderUl.find('img');
-		this.imgWidth = this.sliderImg[0].width;
 		this.imgLenght = this.sliderImg.length;
-		this.sliderDots = this.addElement('._slider__dots', '<div class="slider__dots-line _slider__dots-line" onclick="o2.slider.dotsClick(this)">', this.imgLenght);
-		this.dots = $(this.checkChildren(this.sliderDots,'div')[0]).append('<div class="slider__dots-active _slider__dots-active">');
-		this.dotsActive = $('._slider__dots-active'),
+		this.sliderDots = this.addElement('._slider__dots', '<div class="slider__dots-line _slider__dots-line" onclick="o2.slider.dotsClick(this)"></div>', this.imgLenght);
+		this.sliderDotsLine = $('._slider__dots').find('._slider__dots-line');
+		this.dots = $(this.addChildrenElement(this.sliderDots,'div')[0]).append('<div class="slider__dots-active _slider__dots-active">');
+		this.dotsActive = $('._slider__dots-active');
 		this.dotsWidth =this.dotsActive.width() + 5;
+		this.dotsHover = $('._slider__dots-line');
+
+		this.showPrebiewImg();
 	},
-	sliderRight(value)
+
+	sliderToRight(value)
 	{
+		let imgWidth = this.sliderImg[0].width;
 		this.index++;
-		this.locUl += this.imgWidth;
+		this.locUl = imgWidth * this.index;
 		this.locDots += this.dotsWidth;
 		if(this.index === this.imgLenght)
 		{
@@ -34,15 +40,16 @@ o2.slider =
 		}
 		this.sliderMovement();
 	},
-	sliderLeft(value)
+	sliderToLeft(value)
 	{
+		let imgWidth = this.sliderImg[0].width;
 		this.index--;
-		this.locUl -= this.imgWidth;
+		this.locUl = imgWidth * this.index;
 		this.locDots -= this.dotsWidth;
 		if(this.index < 0)
 		{
 			this.index = this.imgLenght - 1;
-			this.locUl = this.imgWidth * (this.imgLenght - 1);
+			this.locUl = imgWidth * (this.imgLenght - 1);
 			this.locDots = this.dotsWidth * (this.imgLenght - 1);
 		}
 
@@ -51,13 +58,13 @@ o2.slider =
 	dotsClick(value)
 	{
 		let imgWidthDots = this.sliderImg[0].width,
-		dotsWidthDots = this.dotsActive.width() + 5,
-		dots = $(value).data('num');
+			dotsWidthDots = this.dotsActive.width() + 5,
+			dots = $(value).data('num');
 		this.index = dots;
 		this.locUl = imgWidthDots * dots;
 		this.locDots = this.dotsWidth * dots;
 
-		this.sliderMovement()
+		this.sliderMovement();
 	},
 	sliderMovement()
 	{
@@ -67,6 +74,28 @@ o2.slider =
 		this.dotsActive.animate({
 			'margin-left':`${this.locDots}px`
 		}, 500)
+	},
+	showPrebiewImg()
+	{
+		let sliderDots = $('._slider__dots'),
+			sliderDotsLine = this.sliderDotsLine,
+			sliderImg = this.sliderImg.clone(),
+			data;
+		sliderDots.on('mouseover', '._slider__dots-line', function(e) {
+	    	data = ($(e.target).data('num'));
+	    	$(sliderDotsLine[data]).append('<div class="slider__dots-preview _slider__dots-preview">');
+	    	$('._slider__dots-preview').append(sliderImg[data]);
+
+	    	$('._slider__dots-preview').animate({
+	    		'border-radius' : '0',
+	    		'width' : '90px',
+				'height' : '60px'
+	    	}, 80)
+		});
+
+		sliderDots.on('mouseout', '._slider__dots-line', function(e) {
+			$('._slider__dots-preview').remove();
+		});
 	},
 	addElement(parent,clas,length)
 	{
@@ -83,7 +112,7 @@ o2.slider =
 		}
 		return element;
 	},
-	checkChildren(parent, children)
+	addChildrenElement(parent, children)
 	{
 		return $(parent).find(children);
 	}
